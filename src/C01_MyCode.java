d5ef1c397638a14f0853d2001885a7809c453aee
erdembasga <usaerdembas@gmail.com>
2024-08-20 13:52:37 -0400

    V1

---
+++ src/C01_MyCode.java
@@ -0,0 +1,24 @@
+public class C01_MyCode {
+
+    public static void main(String[] args) {
+
+       int a = 1;
+       int b = 4;
+        System.out.println(a+b);
+        System.out.println("Version 01");
+
+        System.out.println("New Line");
+
+        //New Comment
+
+        System.out.println("Version 03");
+
+        //This is Talentify change...
+
+        //Talentify V2
+
+        //Some Changes...
+
+    }
+
+}
